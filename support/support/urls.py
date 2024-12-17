808deb0ee8d25166460b3b853b3baa06f0656ca0
Слава <vyacheslavdobr@gmail.com>
2024-12-17 12:21:48 -0500

    endpoint for market rates added

--- support/support/urls.py
+++ support/support/urls.py
@@ -1,18 +1,20 @@
+import json
 import random
 import string
-import json
 from datetime import datetime, timedelta
-import requests
 
+import requests
 from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.urls import path
 from django.views.decorators.csrf import csrf_exempt
 
-last_answer_time= None 
-total_price=0
+last_answer_time = None
+total_price = 0
+
 
 def create_random_string(size: int) -> str:
     return "".join(random.choice(string.ascii_letters) for _ in range(size))
+
 
 def generate_article_idea(request: HttpRequest) -> HttpResponse:
     content = {
@@ -21,15 +23,16 @@ def generate_article_idea(request: HttpRequest) -> HttpResponse:
     }
     return JsonResponse(content)
 
+
 @csrf_exempt
 def get_current_market_state(request: HttpRequest) -> JsonResponse:
     global last_answer_time, total_price
-    
+
     if request.method == "POST":
         data = json.loads(request.body)
         source: str = data.get("src_currency", "no_currency")
         destination: str = data.get("dest_currency", "no currency")
-        
+
         current_time = datetime.now()
         url = (
             "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&"
@@ -37,29 +40,34 @@ def get_current_market_state(request: HttpRequest) -> JsonResponse:
             f"to_currency={destination}&"
             "apikey=MO31CNEF7DLKTRW1"
         )
-            
+
         if last_answer_time:
             if current_time - last_answer_time < timedelta(seconds=10):
-                return JsonResponse({
-                    "Source": source,
-                    "Destination": destination,
-                    "CACHED_rate": total_price,
-                    "CACHED_time": last_answer_time.strftime("%X"),
-                })
-        
+                return JsonResponse(
+                    {
+                        "Source": source,
+                        "Destination": destination,
+                        "CACHED_rate": total_price,
+                        "CACHED_time": last_answer_time.strftime("%X"),
+                    }
+                )
+
         response: requests.Response = requests.get(url)
-        rate: str = response.json()["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+        rate: str = response.json()["Realtime Currency Exchange Rate"][
+            "5. Exchange Rate"
+        ]
         total_price = rate
         last_answer_time = current_time
-            
+
         return JsonResponse(
             {
-            "Source": source,
-            "Destination": destination,
-            "rate": total_price,
+                "Source": source,
+                "Destination": destination,
+                "rate": total_price,
             }
         )
     return JsonResponse({"error": "Invalid request method"}, status=400)
+
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
