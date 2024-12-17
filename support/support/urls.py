19f5e1f3eacec49653c21b60e0f218e706718300
Слава <vyacheslavdobr@gmail.com>
2024-12-17 12:21:06 -0500

    endpoint for market rates added

--- support/support/urls.py
+++ support/support/urls.py
@@ -1,99 +1,68 @@
 import random
 import string
+import json
+from datetime import datetime, timedelta
+import requests
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest, HttpResponse, JsonResponse
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
+last_answer_time= None 
+total_price=0
 
 def create_random_string(size: int) -> str:
     return "".join(random.choice(string.ascii_letters) for _ in range(size))
 
-
-content = """
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Request rates</title>
-    <link 
-        href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-        rel="stylesheet" 
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" 
-        crossorigin="anonymous"
-    />
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-</head>
-<body>
-    <script>
-        $(document).ready(function () {
-            $("#makeRequest").click(function () {
-                const data = {
-                    src_currency: "USD",
-                    dest_currency: "EUR",
-                };
-                $.ajax({
-                    url: "http://127.0.0.1:8000/rate-check/",
-                    type: "POST",
-                    contentType: "application/json",
-                    data: JSON.stringify(data),
-                    success: function (response) {
-                        console.log("Success:", response);
-                        $("#output").text(JSON.stringify(response, null, 2));
-                    },
-                    error: function (xhr, status, error) {
-                        console.error("Error:", error);
-                        $("#output").text("Error occurred: " + error);
-                    }
-                });
-            });
-            $("#generate").click(function () {
-                $.ajax({
-                    url: "http://localhost:8000/generate-article",
-                    type: "GET",
-                    success: function(data) {
-                        const newItem = `<tr>
-                            <td>${data.title}</td>
-                            <td>${data.description}</td>
-                        </tr>`;
-                        $("#article-ideas").append(newItem);
-                    },
-                    error: function (jqdata, status, error) {
-                        console.log(error);
-                        alert("An error occured while generating an article idea.")
-                    },
-                });
-            });
-        
-        });
-    </script>
-
-
-    <h1>Request rates</h1>
-    <button id="generate" class="btn btn-danger">Fetch information</button>
-    <button id="makeRequest" class="btn btn-primary">Request a rate</button>
-    <pre id="output"></pre>
-
-    <table class="table table-striped">
-        <tr>
-            <th>Title</th>
-            <th>Description</th>
-        </tr>
-        <tbody id="article-ideas"></tbody>
-
-    <br/>
-
-    </table>
-</body>
-</html>
-"""
-
-
 def generate_article_idea(request: HttpRequest) -> HttpResponse:
-    return HttpResponse(content)
+    content = {
+        "title": create_random_string(size=10),
+        "description": create_random_string(size=20),
+    }
+    return JsonResponse(content)
 
+@csrf_exempt
+def get_current_market_state(request: HttpRequest) -> JsonResponse:
+    global last_answer_time, total_price
+    
+    if request.method == "POST":
+        data = json.loads(request.body)
+        source: str = data.get("src_currency", "no_currency")
+        destination: str = data.get("dest_currency", "no currency")
+        
+        current_time = datetime.now()
+        url = (
+            "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&"
+            f"from_currency={source}&"
+            f"to_currency={destination}&"
+            "apikey=MO31CNEF7DLKTRW1"
+        )
+            
+        if last_answer_time:
+            if current_time - last_answer_time < timedelta(seconds=10):
+                return JsonResponse({
+                    "Source": source,
+                    "Destination": destination,
+                    "CACHED_rate": total_price,
+                    "CACHED_time": last_answer_time.strftime("%X"),
+                })
+        
+        response: requests.Response = requests.get(url)
+        rate: str = response.json()["Realtime Currency Exchange Rate"]["5. Exchange Rate"]
+        total_price = rate
+        last_answer_time = current_time
+            
+        return JsonResponse(
+            {
+            "Source": source,
+            "Destination": destination,
+            "rate": total_price,
+            }
+        )
+    return JsonResponse({"error": "Invalid request method"}, status=400)
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
     path(route="generate-article", view=generate_article_idea),
+    path(route="rate-check", view=get_current_market_state),
 ]
