dd4e0f26b44bb5bba2ae5346f32eea0a880aeb55
Viacheslav Dobrovolskiy <vyacheslavdobr@gmail.com>
2025-10-11 14:01:14 +0000

    linters happy

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView
 
 from issues.api import (
@@ -14,6 +17,19 @@ from users.api import (
     UserRetrieveDeleteAPI,
     activate_user,
     resend_activation_mail,
+)
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Snippets API",
+        default_version="v1",
+        description="Test description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="contact@snippets.local"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
 )
 
 urlpatterns = [
@@ -31,4 +47,14 @@ urlpatterns = [
     path("issues/<int:issue_id>/messages", messages_api_dispatcher),
     # Authentication
     path("auth/token/", TokenObtainPairView.as_view()),
+    # Swagger
+    path(
+        "swagger.<format>/", schema_view.without_ui(cache_timeout=0), name="schema-json"
+    ),
+    path(
+        "docs/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
